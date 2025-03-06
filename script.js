f254911b2f2466b35ec51ba2b5323ced6bae838f
Vikas Kumar <95131052+Vikki2021@users.noreply.github.com>
2025-03-06 18:03:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
     //Write your code here
-
+	let first = document.querySelector('[name="fname"]').value.trim()
+	let last = document.querySelector('[name="lname"]').value.trim()
+	alert(`${first} ${last}`)
 }
